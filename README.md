5778d19322e2b4539c9ddee77baf8c47e0ef94d8
zxeenu <zeenu.lastorder@gmail.com>
2025-02-15 02:41:48 +0500

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+```
+npm install
+npm run dev
+```
+
+```
+open http://localhost:3000
+```
+
+```
+curl -X POST http://localhost:3000 \
+     -H "Content-Type: application/json" \
+     -d '{"key": "value"}'
+```
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+const app = new Hono()
+
+app.get('/', async (c) => {
+  const data = await prisma.requestLog.findMany({
+    orderBy: {
+      id: 'desc'
+    }
+  })
+
+  return c.json(data)
+})
+
+app.post('/', async (c) => {
+  const body = await c.req.json()
+
+  await prisma.requestLog.create({
+    data: {
+      'payload': body
+    }
+  })
+
+  return c.text('Done!')
+})
+
+const port = 3000
+console.log(`Server is running on http://localhost:${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
